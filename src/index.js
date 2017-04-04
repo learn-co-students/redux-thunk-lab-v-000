e622374a06ab2311660748ea139ef16fe5946cb8
cmiller36 <m.calliemiller@gmail.com>
2017-04-03 23:10:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+import { WrapperApp} from '../src/App'
 
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk)
+  )
+)
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <WrapperApp />
+  </Provider>,
     document.getElementById('root')
 );
