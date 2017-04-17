313be294aa127cad23f1693e1f9cb4a97a3cf0da
Scott Young <young.scottw@gmail.com>
2017-04-17 18:19:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import {Provider, createStore, applyMiddleware} from 'redux'
 import App from './App'
+import thunk from 'redux-thunk';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
