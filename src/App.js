40220a64094ccbd4b10276fb4a728855e9923d90
Scott Savage <snsavage@gmail.com>
2017-06-05 17:50:45 -0400

    Pass all lab specs

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
 
-class App extends Component {   
-  
+import { bindActionCreators } from 'redux'
+
+import { connect } from 'react-redux';
+import CatList from './CatList';
+
+import { fetchCats } from './actions/catActions';
+
+export class App extends Component {
+  componentDidMount() {
+    this.props.fetchCats();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,12 +23,20 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics} />
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    catPics: state.cats.pictures
+  }
+}
 
+const mapDispatchToProps = (dispatch) => {
+  return {fetchCats: bindActionCreators(fetchCats, dispatch)};
+}
 
-export default App
-
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/actions/catActions.js
@@ -0,0 +1,13 @@
+import fetch from 'isomorphic-fetch';
+
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:3000/db')
+      .then(response => response.json())
+      .then(pictures => dispatch({
+        type: 'FETCH_CATS',
+        payload: pictures.images
+      }));
+  };
+}
