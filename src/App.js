b16a14eeec387fce91aeecad283ded562f4440b7
nothingisfunny <if.the.kids.r.united@gmail.com>
2017-07-18 10:35:37 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import {connect} from 'react-redux'
+import CatList from './CatList'
+import { bindActionCreators } from 'redux'
+import * as actions from './actions/catActions.js'
 
-class App extends Component {   
-  
+class App extends Component {  
+
+  componentDidMount(){
+    this.props.actions.fetchCats();
+  }
+
   render() {
+    console.log(this.props.cats)
     return (
       <div className="App">
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">CatBook</a>
+            <a href="#">CatBook</a>
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics} />
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats.pictures
+  }
+}
 
+function mapDispatchToProps(dispatch){
+  return {actions: bindActionCreators(actions, dispatch)}
+}
 
-export default App
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
---
+++ src/actions/catActions.js
@@ -0,0 +1,11 @@
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({type: 'LOADING_CATS'})
+    return fetch('http://localhost:3000/db')
+      .then(response => {return (response.json())})
+      .then(response => {
+        dispatch({type: 'FETCH_CATS', payload: response.images});
+      })
+
+  };
+}
