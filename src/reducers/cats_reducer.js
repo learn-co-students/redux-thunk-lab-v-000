3784c176c4342df049917469ba2d7eee4b416345
Sholom Steinmetz <boruchsteinmetz@outlook.com>
2017-07-25 14:38:20 -0400

    Done.

--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -2,7 +2,7 @@ const catsReducer = (state = { loading: false, pictures: [] }, action) => {
   switch (action.type) {
     case 'FETCH_CATS':
     console.log(state, "our state")
-      return Object.assign({}, state, {pictures: state.pictures.concat(action.payload) })
+      return Object.assign({}, state, {pictures: state.cats.concat(action.payload) })
       default:
         return state
   }
