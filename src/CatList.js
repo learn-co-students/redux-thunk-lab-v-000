d2d2467a7fb8403fd48fc57a4919e4e6b2bd219b
denisedearman <dearmandenise@gmail.com>
2017-09-07 18:44:50 -0400

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const CatList = (props) => {
+     return(
+       <div>
+         { props.catPics.map(cat => <div><img alt={cat.source_url} key={cat.id} url={cat.url}/></div>)}
+       </div>
+     );
+
+}
+
+export default CatList
