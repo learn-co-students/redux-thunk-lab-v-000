878c426d6d22c6f54c4072d4c9a94190bd7d0876
Mike Etlinger <mk-etlinger@gmail.com>
2017-09-22 11:33:12 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from 'react-dom';
+import App from './App';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import rootReducer from './reducers/index';
 
+
+function configureStore(){
+    return createStore(
+        rootReducer,
+        applyMiddleware(thunk)        
+    );
+}
+
+const store = configureStore();
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
