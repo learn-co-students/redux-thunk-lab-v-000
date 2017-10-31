d35d29157d6ad1317d02348e9d8e8f531c8e562c
mikka <mikkanthrope@gmail.com>
2017-10-30 21:49:52 -0700

    Done.

--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -5,6 +5,8 @@ export default function catsReducer(state = {loading: false, cats: []}, action =
       if (action.pictures) {
         return {
             loading: false,
+            // use spread operator to explode the action.pictures array into its component elements
+            // instead of nesting array inside the cats array
             cats: [...state.cats, ...action.pictures]
           }
       } else {
