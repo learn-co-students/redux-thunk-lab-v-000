1a438c2f9a4e63bad6df580d82586f560836a582
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-11-01 18:51:08 -0400

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,7 +1,9 @@
+import fetch from 'isomorphic-fetch'
 
 export function fetchCats(){
   return (dispatch)=>{
-    return fetch('http://127.0.0.1:4000/db').then(res => res.json())
+    dispatch({type: 'LOADING_CATS'})
+    return fetch('http://localhost:4000/db').then(res => res.json())
       .then(res => {return dispatch({type: 'FETCH_CATS', payload: res.images})});
   }
 }
--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -2,6 +2,8 @@ export default function catsReducer(state={loading: false, pictures: []}, action
     switch(action.type){
       case 'FETCH_CATS':
         return {loading: false, pictures: action.payload};
+      case 'LOADING_CATS':
+        return {loading: true, pictures: []};
       default:
         return state;
     }
