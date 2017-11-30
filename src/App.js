16c542563a1c7edd66d24bdaa42867f29114e078
Bryan Hou <bryan.hou94@gmail.com>
2017-11-30 18:56:13 -0500

    seems to be working as per the description but not passing 3 tests

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import CatList from './CatList';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as actions from './actions/catActions';
 
-class App extends Component {   
-  
+class App extends Component {
+
+  componentDidMount() {
+    // debugger;
+    this.props.actions.fetchCats();
+  }
+
   render() {
     return (
       <div className="App">
@@ -13,12 +22,18 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics}/>
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {catPics: state.cats.pictures}
+}
 
+function mapDispatchToProps(dispatch) {
+  return {actions: bindActionCreators(actions, dispatch)}
+}
 
-export default App
-
+export default connect(mapStateToProps, mapDispatchToProps)(App);
