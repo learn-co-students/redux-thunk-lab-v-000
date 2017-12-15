fe1e85bb8dc9cba49bd3dbefac73520555223275
Lita Stephenson <Lita.Stephenson@gmail.com>
2017-12-14 19:49:38 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-
+import { createStore } from 'react-redux'
+import rootReducer from './reducers'
+import {Provider} from 'react-redux'
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+  <Provider store={store}>
+    <WrapperApp />
+  </Provider>,
+  document.getElementById('root')
 );
