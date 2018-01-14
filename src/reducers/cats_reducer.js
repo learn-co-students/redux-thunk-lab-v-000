a2c7fbba7c92f4fd58725e6501c2e189c428816d
burrahey <burra.archana@gmail.com>
2018-01-13 21:45:59 -0500

    Done.

---
+++ src/reducers/cats_reducer.js
@@ -0,0 +1,12 @@
+const catsReducer = function(state={loading: false, pictures: []}, action){
+  switch(action.type){
+    case 'FETCH_CATS':
+      return { loading: false, pictures: action.payload };
+    case 'LOADING_CATS':
+      return { ...state, loading: true }
+    default:
+      return state;
+  }
+}
+
+export default catsReducer;
