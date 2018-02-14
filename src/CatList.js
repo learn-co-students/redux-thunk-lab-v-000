001883da4fffce2e1ec515ae0a29a838fc6a291a
ndalcin <dalcin.nicole@gmail.com>
2018-02-14 08:22:59 -0500

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const CatList = ({catPics}) => {
+  function listCats() {
+    return catPics.map(catPic => {
+      return (
+        <div className="col-lg-12">
+          <div className="col-lg-6 col-lg-offset-3 well">
+            <img src={catPic.url} key={catPic.url} className="thumbnail responsive" style={{height: '220px', width: '221px', margin: 'auto'}}/>
+          </div>
+        </div>
+      )
+    })
+  }
+  return (
+    <div>
+      {listCats()}
+    </div>
+  )
+}
+
+export default CatList;
