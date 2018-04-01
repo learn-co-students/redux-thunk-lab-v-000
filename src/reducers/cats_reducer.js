acfea7e57f0cca5d7669671fb18cd15544fe4bc8
Denis Atkesone <denis.atkesone@gmail.com>
2018-04-01 12:36:19 +0200

    Done.

--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -1,5 +1,7 @@
-function catsReducer(state = { loading: false, pictures: [] }, action) {
+export default function catsReducer(state = { loading: false, pictures: [] }, action) {
   switch (action.type) {
+    case 'LOADING_CATS':
+      return Object.assign({}, state, { loading: true })
     case 'FETCH_CATS':
       return {
         loading: false,
@@ -9,5 +11,3 @@ function catsReducer(state = { loading: false, pictures: [] }, action) {
       return state;
   }
 }
-
-export default catsReducer
