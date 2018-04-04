cf31b414bd0c1d016cf8b2cce1ad87c99992b48d
streamedLine <dovidlwtn@gmail.com>
2018-04-04 11:43:20 -0400

    Done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,15 @@
+import fetch from 'isomorphic-fetch'
+
+export const fetchCats = () => {
+	return (dispatch) => {
+		dispatch({type: 'LOADING_CATS'})
+
+		return fetch('http://localhost:4000/db').then(response => {
+			return response.json();
+		})
+		.then(catsJSON => {
+			dispatch({type: 'FETCH_CATS', payload: catsJSON.images})
+		});
+	}
+
+}
