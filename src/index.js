cfd38f2426eea78f6347a288aebd2ecfef8cc824
Oski <github email address>
2018-04-19 14:48:32 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
+import rootReducer from './reducers/index'
+import { WrapperApp } from './App'
 
+const store = createStore(rootReducer, applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
-    <App />,
+<Provider store={store} >
+    <WrapperApp />
+</Provider>,
     document.getElementById('root')
 );
