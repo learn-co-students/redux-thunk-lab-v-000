968be32ef8b3b3d80032c4a3c1b8ea6acbd7fd99
PRbsas <paularamirez0316@gmail.com>
2018-05-03 16:23:58 -0400

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const CatList = (props) => {
+  function listCats () {
+    return props.catPics.map(catPic => {
+      return (
+        <div>
+          <div>
+            <img src={catPic.url} alt='Cat Pic' />
+          </div>
+        </div>
+      )
+    })
+  }
+  return (
+    <div>
+      { listCats() }
+    </div>
+  )
+}
+
+export default CatList
