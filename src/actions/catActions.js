ea9f63186bf557506fcb320f2759bdb8d900888c
mbalsamo <github email address>
2018-05-22 14:19:48 -0400

    done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,10 @@
+import fetch from 'isomorphic-fetch'
+
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:4000/db')
+      .then(response => response.json())
+      .then(cats => dispatch({ type: 'FETCH_CATS', payload: cats.images }));
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+import { WrapperApp } from './App'
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers/index';
+import { applyMiddleware, createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store} >
+      <WrapperApp />
+    </Provider >,
     document.getElementById('root')
 );
