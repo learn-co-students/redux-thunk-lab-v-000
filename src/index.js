6b2c332712313df6e5d0f4a19b59788068d6bc27
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-06-05 23:30:50 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {createStore, applyMiddleware, compose } from 'redux'
-import { browserHistory } from 'react-router'
 import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 import { WrapperApp } from './App'
