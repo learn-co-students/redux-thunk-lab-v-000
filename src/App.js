5e8b8588a1f2e687bc413ea51b4b882c20c2e1c5
jessiehuff <huffjd1@gmail.com>
2018-06-10 19:10:39 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import {connect} from 'react-redux'; 
+import { bindActionCreators } from 'redux';
+import {fetchCats} from './actions/catActions'; 
+import CatList from './CatList';
 
-class App extends Component {   
+export class App extends Component {   
+  
+  componentDidMount() {
+    if (this.props.catPics.length === 0) {
+      console.log('Cats can play fetch too!')
+      this.props.actions.fetchCats()
+    }
+  }
   
   render() {
     return (
@@ -10,15 +21,25 @@ class App extends Component {
           <Navbar.Header>
             <Navbar.Brand>
               <a href="#">CatBook</a>
+              <CatList catPics={this.props.catPics} /> 
             </Navbar.Brand>
           </Navbar.Header>
-        </Navbar>
-      </div>
+        </Navbar> 
+      </div> 
     );
   }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    catPics: state.cats.pictures
+  };
 }
 
+const mapDispatchToProps = (dispatch) => {
+  bindActionCreators({
+    fetchCats: fetchCats}, dispatch);
+};
 
-
-export default App
+export const WrapperApp = connect(mapStateToProps, mapDispatchToProps)(App);
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
-import App from './App'
+import WrapperApp from './App'
+import {Provider} from 'react-redux'; 
+import { createStore, applyMiddleware, compose } from 'redux'; 
+import thunk from 'redux-thunk'; 
+import rootReducer from './reducers/index'; 
+
+const store = createStore(rootReducer, applyMiddleware(thunk)); 
 
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root')
+    <Provider store={store} > 
+        <WrapperApp />
+    </Provider>, document.getElementById('root')
 );
