ef716b4db9c331ab666257e054986381bb6a0f69
Penguino <heriberto@melo.nyc>
2018-06-20 10:50:42 -0400

    Done.

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import rootReducer from './reducers/index'
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+
+export function configureStore(){
+  return createStore(rootReducer,
+  	compose(applyMiddleware(thunk), window.devToolsExtension ? window.devToolsExtension() : f => f));
+}
+
+export const store = configureStore()
