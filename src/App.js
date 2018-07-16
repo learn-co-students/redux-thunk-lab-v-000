1362a1a6811269b2c15af6e9e4f81323fedf08b4
github username <github email address>
2018-07-16 18:10:40 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import CatList from './CatList'
+import { connect } from 'react-redux';
+import fetchCats from './actions/catActions';
 
-class App extends Component {   
+// import the action creator fetchCats. send it through the connect function below to call it from props, wrapped in the dispatch.
+
+class App extends Component {
+  
+  componentDidMount() {
+    this.props.fetchCats();
+  }
   
   render() {
     return (
@@ -13,12 +22,17 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.cats} />
       </div>
     );
   }
 }
 
+const mapStateToProps = (state) => {
+  return {
+    cats: state.cats,
+  }
+}
 
-
-export default App
+export default connect(mapStateToProps, { fetchCats })(App)
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import { Provider } from 'react-redux'
+import store from './configureStore'
+// import the store variable and the Provider and use them below
 
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store} >
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
