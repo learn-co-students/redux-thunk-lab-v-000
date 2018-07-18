6ae83623b2f10d32170c4839272408e6cd1d8439
Stephen Thurman <33437392+thurman024@users.noreply.github.com>
2018-07-18 17:02:11 -0500

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const CatList = ({ catPics }) => {
+  function listCats() {
+    return catPics.map(catPic => {
+      return (
+        <div><img src={catPic.url}/></div>
+      )
+    })
+  }
+  return (
+    <div>
+    {listCats()}
+    </div>
+  )
+}
+
+export default CatList
