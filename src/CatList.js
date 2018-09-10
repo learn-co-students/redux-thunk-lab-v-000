456c898a3d5f554fb6c56270ae4fcf39af73dcd5
Aspen James <aspenjames@tqca.org>
2018-09-10 14:59:22 -0700

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class CatList extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.renderCats()}
+      </div>
+    );
+  };
+
+  renderCats = () => {
+    return this.props.catPics ? this.props.catPics.map(cat => <img key={cat.id} src={cat.url} alt={cat.id} />) : null
+  }
+}
+
+export default CatList;
