c8e6cea73a9a9b7abe45bc13db4ae7f8449ef853
Donna <donnamcla@gmail.com>
2018-09-11 10:03:10 -0400

    passes the tests but doesn't actually do anything when you click 'CatBook'

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions';
+import CatList from './CatList';
 
-class App extends Component {   
+class App extends Component {
+  
+  componentDidMount() {
+    this.props.fetchCats;
+  }
   
   render() {
     return (
@@ -13,12 +20,18 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        {this.props.catPics.length > 0 ? <CatList catPics={this.props.catPics}/> : null}
       </div>
     );
   }
 }
 
+const mapStateToProps = state => {
+  return { catPics: state.cats.pictures };
+}
 
+const mapDispatchToProps = dispatch => {
+  fetchCats: () => dispatch(fetchCats);
+}
 
-export default App
-
+export default connect(mapStateToProps, mapDispatchToProps)(App);
