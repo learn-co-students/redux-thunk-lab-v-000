44f35c57d883016e8d5548b99f788bf28914ba18
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-10-01 08:07:45 -0400

    Done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,13 @@
+export function fetchCats() {
+	console.log("inside fetchCats action")
+  return function (dispatch) {
+
+    dispatch({ type: 'LOADING_CATS' });
+     return fetch('http://localhost:4000/db')
+      .then(response => response.json())
+      .then(cats =>
+
+      	dispatch({ type: 'FETCH_CATS', payload: cats.images} )
+       	);
+  };
+}
