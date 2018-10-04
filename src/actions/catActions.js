d148f8bf8a9e2594fcde8825be3f2076b96d97d1
github username <github email address>
2018-10-04 15:41:53 -0400

    Done.

---
+++ src/actions/catActions.js
@@ -0,0 +1,9 @@
+
+export function fetchCats() {
+return (dispatch) => {
+  dispatch({ type: 'LOADING_CATS' });
+  return fetch('http://localhost:4000/db')
+    .then(response => response.json())
+    .then(cats => dispatch({ type: 'ADD_CATS', payload: cats}));
+};
+}
