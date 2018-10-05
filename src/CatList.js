f9e19f781579019d7eb811e1c5bcea94de030b30
unenlightened <adept_inept@yahoo.com>
2018-10-05 18:41:40 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
 
 class CatList extends Component {
-
   render() {
-    const renderCatPics = this.props.catPics.map(catPic => <img src={catPic} />)
+    const renderCatPics = this.props.catPics.map(catPic => <img src={catPic} alt='cat' />)
 
     return (
       <div>
