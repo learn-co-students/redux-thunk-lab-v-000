89e53cfa202f0b18c3992ff2cf00e8c125309b3e
Chely Ho <adept_inept@yahoo.com>
2018-10-05 22:45:53 +0000

    Done.

--- src/reducers/cats_reducer.js
+++ src/reducers/cats_reducer.js
@@ -1,5 +1,10 @@
 function catsReducer(state = [], action) {
   switch (action.type) {
+    case 'LOADING_CATS':
+      return state
+
+    case 'FETCH_CATS':
+      return action.cats
 
     default:
       return state
