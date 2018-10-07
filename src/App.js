cc447fae8503821a26a5db3144aca572f88b4236
e-be-walk <e.be.walk@gmail.com>
2018-10-07 14:05:22 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions';
+import CatList from './CatList';
 
-class App extends Component {   
-  
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">CatBook</a>
+                <CatList catPics={this.props.catPics} />
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
       </div>
     );
   }
+};
+
+
+const mapStateToProps = state => {
+  return { catPics: state.cats.pictures }
 }
 
-
-
-export default App
-
+export default connect(mapStateToProps, {fetchCats})(App);
---
+++ src/reducers/cats_reducer.js
@@ -0,0 +1,18 @@
+export default function catsReducer(
+  state = {
+    loading: false,
+    pictures: []
+  }, action) {
+
+    switch(action.type){
+      case "LOADING_CATS":
+        return Object.assign({}, state, {loading: true})
+
+      case "FETCH_CATS":
+        console.log(action)
+        return {...state, loading: false, pictures: action.payload.map(cat => cat )}
+
+      default:
+        return state
+    }
+  }
