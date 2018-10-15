d4ec597725b3ece5fcbeba90b525f4567dcfe6c3
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-15 00:29:05 -0400

    Use combineReducers to define rootReducers and use the rootReducers to initialize the store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import catsReducer from './reducers/catsReducer'
+import rootReducer from './reducers/index'
 
-const store = createStore(catsReducer)
+const store = createStore(rootReducer)
 
 ReactDOM.render(
     <Provider store={store} >
