eceba8fc9561800779a2e7a72f71e25bec9342ea
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-10-15 01:25:17 -0400

    Add logging to reducer actions. Implement storage of retrieved data

--- src/reducers/catsReducer.js
+++ src/reducers/catsReducer.js
@@ -1,10 +1,13 @@
 export default function catsReducer(state = { cats: [] }, action){
     switch(action.type){
         case 'LOADING_CATS':
+            console.log("LOADING_CATS called")
             return state
 
         case 'FETCH_CATS':
-            return state
+            console.log("FETCH_CATS called")
+            console.log(action)
+            return {...state, cats: action.payload}
 
         default:
             return state
