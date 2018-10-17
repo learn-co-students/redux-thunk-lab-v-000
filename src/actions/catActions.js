83e017afd08a7d7a01dabfbb560cda883e8b4920
badlychadly <chadmontoya21@yahoo.com>
2018-10-17 13:47:46 -0700

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -6,15 +6,12 @@ export function fetchCats() {
         
         dispatch({type: "LOADING_CATS"})
         
-        return fetch('http://10.0.0.99:4000/db')
+        return fetch('http://localhost:4000/db')
         .then(response => response.json())
         .then(responseJSON => dispatch({type: "FETCH_CATS", payload: responseJSON.images}))
 
-        
-        // fetch('http://localhost:4000/db').then(response => {
-        // return response.json()
-        // }).then(responseJSON => {
-        // console.log(responseJSON)
-        // })
+        // return fetch('http://10.0.0.99:4000/db')
+        // .then(response => response.json())
+        // .then(responseJSON => dispatch({type: "FETCH_CATS", payload: responseJSON.images}))
     }
 }
