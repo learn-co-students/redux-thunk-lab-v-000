264002d7a5c204d354786dbe0c2cae954ffb7264
Monica Tamaru Jones <mntamaru@gmail.com>
2018-12-10 17:16:55 +0000

    Done.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
 
-class App extends Component {   
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
+
+class App extends Component { 
+  
+  // componentDidMount will guarantee that there’s a component to update.
+
+  componentDidMount(){
+    this.props.fetchCats()
+  }
   
   render() {
+    
+    const cats = this.props.cats.map(cat => <li key={cat.id}>{cat.name}</li>);
+    
     return (
       <div className="App">
         <Navbar>
@@ -13,12 +26,21 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        
+        <CatList catPics={this.props.catPics} />
+        
       </div>
     );
   }
 }
 
+function mapStateToProps(state){
+  return {catPics: state.catReducer.pictures}
 
+}
 
-export default App
+function mapDispatchToProps(dispatch){
+  return { fetchCats: () => dispatch(fetchCats()) }
+}
 
+export default connect(mapStateToProps, mapDispatchToProps)(App)
