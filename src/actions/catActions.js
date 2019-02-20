d19a2ed8505efdbeff42f149bd1f002986f54147
Vibraniumforge <gregm9999@gmail.com>
2019-02-20 13:57:35 -0800

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,14 +1,12 @@
 export function fetchCats() {
+  console.log("fetchCats fires");
   return dispatch => {
     dispatch({ type: "LOADING_CATS" });
     return fetch("http://localhost:4000/db")
       .then(response => {
         return response.json();
       })
-      .then(responseJSON => {
-        return responseJSON.images;
-      })
-      .then(cats => dispatch({ type: "FETCH_CATS", payload: cats }))
-      .catch(err => console.error("Error"));
+      .then(cats => dispatch({ type: "FETCH_CATS", payload: cats.images }))
+      .catch(err => console.error("Error", err));
   };
 }
