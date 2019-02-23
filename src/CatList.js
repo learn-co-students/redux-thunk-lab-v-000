6e878c50700c4c073e9ab36af958a3b2fda33c16
Mallory <mallory.feuer@gmail.com>
2019-02-23 15:52:18 -0500

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class CatList extends Component {
+
+  render() {
+    return (
+      <div>
+        {this.props.catPics.map((pic) =>
+          <img src={pic.url}/>
+        )}
+      </div>
+    );
+  }
+}
+
+
+export default CatList
---
+++ src/actions/catActions.js
@@ -0,0 +1,9 @@
+export function fetchCats() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS' });
+    return fetch('http://localhost:4000/db')
+      .then(response => {return response.json() })
+      .then(responseJSON => {return responseJSON.images})
+      .then(payload => dispatch({ type: 'FETCH_CATS', payload }));
+  };
+}
