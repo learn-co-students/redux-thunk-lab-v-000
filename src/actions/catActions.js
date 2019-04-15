19a80b7e4ba02ec14f5cc156de71b8b71bde931d
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-04-15 16:32:19 -0400

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,24 +1,15 @@
-// export function fetchCats() {
-//   const url = 'http://localhost:4000/db'
-//   return (dispatch) => {
-//     dispatch({ type: 'LOADING_CATS'})
-//     return fetch(url)
-//       .then(response => response.json())
-//       .then(cats => {
-//         const catPics = cats.images
-//         dispatch({ type: 'FETCH_CATS', payload: catPics })
-//       }
-//     )
-//   }
-// }
-
 export function fetchCats() {
-  return (dispatch) => {
-      dispatch({ type: 'LOADING_CATS' });
+  const url = 'http://localhost:4000/db'
 
-      return fetch('http://localhost:4000/db')
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_CATS'})
+    
+    return fetch(url)
       .then(response => response.json())
-      .then(things => things.images)
-      .then(cats => dispatch({type: 'FETCH_CATS', payload: cats}))
-    }
+      .then(cats => {
+        const catPics = cats.images
+        dispatch({ type: 'FETCH_CATS', payload: catPics })
+      }
+    )
+  }
 }
