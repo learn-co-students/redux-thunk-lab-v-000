4793b8a15bb7a1aeb7dbc5ef47bd9b2ad39b33c4
Guess <guess@Guesss-iMac.local>
2019-07-09 10:52:19 -0400

    Done.

---
+++ src/reducers/cats_reducer.js
@@ -0,0 +1,21 @@
+export default function catsReducer(state = { loading: false, pictures: [] }, action) {
+    switch (action.type) {
+        case "LOADING_CATS":
+
+            return {
+                ...state,
+                loading: true,
+            }
+        case "FETCH_CATS":
+
+            return {
+                ...state,
+                loading: false,
+                pictures: action.payload
+            }
+
+        default:
+            return state
+    }
+
+}
