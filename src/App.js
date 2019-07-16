7cdd11e73db50a5b80ab3d10b1c7dbbaa518dc46
github username <github email address>
2019-07-16 16:01:44 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import {Navbar} from 'react-bootstrap'
 
+import { connect } from 'react-redux'
+import { fetchCats } from './actions/catActions'
+import CatList from './CatList'
+
 class App extends Component {   
   
+  componentDidMount() {
+    this.props.fetchCats()
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,6 +18,7 @@ class App extends Component {
           <Navbar.Header>
             <Navbar.Brand>
               <a href="#">CatBook</a>
+              <CatList catPics={this.props.catPics} />
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
@@ -18,7 +27,10 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return { catPics: state.catsReducer.pictures }
+}
 
 
-export default App
+export default connect(mapStateToProps, { fetchCats }) (App)
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import rootReducer from './reducers/index'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 
 ReactDOM.render(
+	<Provider store={store}>
     <App />,
-    document.getElementById('root')
+    </Provider>,
+    document.getElementById('root') 
 );
