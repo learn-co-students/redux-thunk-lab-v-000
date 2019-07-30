f9a624eebae3cc7382aba306e78e7b979af82c8e
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-30 17:57:51 -0400

    incorporate middleware in initialized store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
