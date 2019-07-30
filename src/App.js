b90bda5da76e6b17d687aee2539ce71f3cc64170
MargaritaKhodzka <mkhodzka@gmail.com>
2019-07-30 17:54:37 -0400

    App use connect and mapStateToProps to set a prop of catPics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import {bindActionsCreators} from 'redux';
+import * as actions from './actions/catActions.js';
 
-class App extends Component {   
-  
+class App extends Component {
+
+  componentDidMount() {
+    if (this.props.catPics.length === 0) {
+      this.props.actions.fetchCats()
+    }
+  }
+
   render() {
     return (
       <div className="App">
@@ -18,7 +27,12 @@ class App extends Component {
   }
 }
 
+const mapStateToProps = state => {
+  return {catPics: state.cats.pictures}
+};
 
+const mapDispatchToProps = dispatch => {
+  return {actions: bindActionsCreators(action, dispatch)}
+};
 
-export default App
-
+export default connect(mapStateToProps, mapDispatchToProps)(App);
