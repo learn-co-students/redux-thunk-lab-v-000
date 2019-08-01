bdf4d2b79a4c0cda54185b97151d139085cc1b2c
github username <github email address>
2019-08-01 12:31:40 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import { connect } from 'react-redux'
+import { fetchCats }from './actions/catActions'
+import CatList from './CatList'
 
-class App extends Component {   
-  
+
+
+class App extends Component {
+
+  componentDidMount = () => {
+     this.props.fetchCats();
+  }
+
   render() {
+console.log(this.state)
     return (
       <div className="App">
         <Navbar>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="#">CatBook</a>
+            <CatList catPics={this.props.catPics} />
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+
       </div>
+
     );
   }
 }
 
 
+const mapStateToProps = (state) => {
+  return {
+  catPics: state.pictures
+  }
+}
 
-export default App
-
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: fetchCats
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
