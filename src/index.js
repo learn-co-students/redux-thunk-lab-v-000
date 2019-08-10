c122ffa04812a4581c7988d7c4bdef9a4bb1bb60
Rachel Wyatt <rw126@evansville.edu>
2019-08-10 10:22:11 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
-import { combineReducers } from 'redux'
-import catsReducer from './cats_reducer'
 import {createStore, applyMiddleware} from 'redux'
 import { Provider } from 'react-redux'
 import rootReducer from './reducers/index'
 import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
-
-const rootReducer = combineReducers({
-    cats: catsReducer
-})
-
 
 ReactDOM.render(
     <Provider store={store}>
