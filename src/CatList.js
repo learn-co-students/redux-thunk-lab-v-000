ca8bd1812f8d00320b82c25e7ff488f611886707
Ashley <ashleywong678@gmail.com>
2019-08-28 14:56:55 -0400

    Done.

---
+++ src/CatList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class CatList extends Component {
+
+  render(){
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default CatList
