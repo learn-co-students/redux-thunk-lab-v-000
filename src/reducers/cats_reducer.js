69b8d44793657aea1a19ec33a1dbb0d253fbac42
Wendy Calderon <wendycalderon@gmail.com>
2019-08-30 19:40:09 -0400

    Set initial structure for catsReduce

---
+++ src/reducers/cats_reducer.js
@@ -0,0 +1,18 @@
+export default function catsReducer(state = {}, action) {
+    switch (action.type) {
+        case 'LOADING_CATS':
+        
+        return {
+          
+        }
+  
+        case 'FETCH_CATS':
+
+        return { 
+
+        }
+
+        default:
+            return state;
+    }
+};
