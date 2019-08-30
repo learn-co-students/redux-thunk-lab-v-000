247f4eed38f2cdd47604b78ec54bea1e29634baf
nmarini <nmarini44@gmail.com>
2019-08-29 18:41:14 -0700

    Done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
-import {Navbar} from 'react-bootstrap'
+import {Navbar} from 'react-bootstrap';
+import { connect } from 'react-redux';
+import CatList from './CatList';
+import { fetchCats } from './actions/catActions'
 
-class App extends Component {   
+class App extends Component {  
+  
+  componentDidMount() {
+    this.props.fetchCats
+  }
   
   render() {
     return (
@@ -13,12 +20,20 @@ class App extends Component {
             </Navbar.Brand>
           </Navbar.Header>
         </Navbar>
+        <CatList catPics={this.props.catPics}/>
       </div>
     );
   }
 }
 
 
+const mapStateToProps = state => ({
+  catPics: state.cats.pictures
+})
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  fetchCats: dispatch(fetchCats())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
 import App from './App'
 
+const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  )
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
