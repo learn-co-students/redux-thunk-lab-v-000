0eafdf486d532a69ad24c86405a24d7dc586fa8b
Wendy Calderon <wendycalderon@gmail.com>
2019-08-30 18:38:53 -0400

    Initialize store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
+import cats_reducer from './reducers/cats_reducer'
 
+//use Redux's createStore function to initialize your store
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+const store = createStore(cats_reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
