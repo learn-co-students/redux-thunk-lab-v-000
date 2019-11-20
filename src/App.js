0c8d5370af067763e7c8990676547371b1d62914
github username <github email address>
2019-11-20 15:01:39 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React, { Component } from 'react';
+import CatList from './CatList'
+import { connect } from 'react-redux';
+import { fetchCats } from './actions/catActions'
+ 
+class App extends Component {
+ 
+  componentDidMount() {
+    console.log(this.props)
+    this.props.fetchCats()
+  }
+ 
+  handleLoading = () => {
+    console.log(this.props.loading)
+    if(this.props.loading) {
+      return <div>Loading...</div>
+    } else {
+      return <CatList catPics={this.props.catPics} />
+    }
+  }
 
-class App extends Component {   
-  
   render() {
     return (
-      <div>
+      <div className="App">
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        {this.handleLoading()}
       </div>
     );
   }
 }
+ 
+const mapStateToProps = state => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+}
+ 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
-export default App
 
+// normal way of rendering without loading function:
+// render() {
+//   console.log(this.props.catPics) // log will fire every time App renders
+//   console.log(this.props.loading)
+//   return (
+//     <div className="App">
+//       <h1>CatBook</h1>
+//       {/* missing component */}
+//       <CatList catPics={this.props.catPics}/>
+    
+//     </div>
+//   );
+// }
+// }
