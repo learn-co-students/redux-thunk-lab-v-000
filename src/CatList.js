15017adedc7b6409684aa43846aa90ff587d9560
Pamela Frieze Torres-Rocca <pamela@Morpheus.local>
2019-12-16 22:27:57 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,15 @@
 // write your CatList component here
+import React, { Component } from 'react';
+
+
+class CatList extends Component {
+
+	render() {
+    return(
+    	<div>
+    	</div>
+    	);
+    }
+};
+
+export default CatList;
