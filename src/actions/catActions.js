525a596b3a41d77d3ea6c2cdcb5c0984e2850ffd
iamtamtamiam <vutam_a@hotmail.com>
2020-07-23 15:57:38 -0500

    Done.

--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,17 @@
 // write and export your action creator function here
+
+//Thunk alters the behavior of action creator functions, 
+//  allowing us to return a function that takes in dispatch. 
+//Inside this function, we can execute asynchronous code, and, once resolved, 
+//  we can use dispatch to update our store with the remote data.
+
+export const fetchCats = () => {
+    return (dispatch) => {
+      dispatch({ type: 'LOADING_CATS'})
+      fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+        return response.json()
+      }).then(responseJSON => {
+        dispatch({ type: 'ADD_CATS', cats: responseJSON.images })
+      })
+    }
+  }
