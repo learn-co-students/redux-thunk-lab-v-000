4c339d861ff81ea4dffc860efa8957d34fabdd3b
github username <github email address>
2020-12-27 19:09:38 -0800

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,62 @@
 // write your CatList component here
+
+
+// import React from 'React';
+import React, {Component} from 'React';
+
+
+// this.props in CatList................ {
+//   catPics: [
+//     { id: 1, url: 'www.example.com/cat1' },
+//     { id: 2, url: 'www.example.com/cat2' }
+//   ]
+// }
+// this.props.catPics in CatList................ [
+//   { id: 1, url: 'www.example.com/cat1' },
+//   { id: 2, url: 'www.example.com/cat2' }
+// ]
+
+// {this.props.catPics.map((item, index) => (<img src={item.url} />))}
+
+class CatList extends React.Component {
+  render(){
+    // console.log("this.props in CatList................", this.props)
+    // console.log("this.props.catPics in CatList................", this.props.catPics)
+
+    // const catImages =
+
+    return(
+      <div>
+        {this.props.catPics.map((item, index) => (<img src={item.url} />))}
+      </div>
+    )
+  }
+};
+
+export default CatList;
+
+
+// notes/approaches
+
+// 1
+// making a const variable with the images, and then mapping over them.
+// notes- have to learn how to iterate over an object with map. what do the keys/index
+// things mean exactly
+
+
+
+
+
+//
+// const CatList = (props) => {
+//   <div> this.props.map(item => {
+//     <img>item </img>
+//   }) </div>
+// }
+// <img item />
+//
+// <div> this.props.catPics.map(item => {
+//   <img item />
+// }) </div>
+//
+// export default CatList;
---
+++ src/questions.js
@@ -0,0 +1,8 @@
+
+
+// fetch('https://learn-co-curriculum.github.io/cat-api/cats.json').then(response => {
+//   return response.json()
+// }).then(responseJSON => {
+//   // instead of logging here, call dispatch and send the cat JSON data to your store
+//   console.log(responseJSON.images)
+// })
