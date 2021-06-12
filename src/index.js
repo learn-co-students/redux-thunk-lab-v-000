cc88d0af0628ac586417b6bc3d3f50b1ecf0b589
emanao <emanao@me.com>
2021-06-12 11:08:34 -0400

    Initial setup

--- src/index.js
+++ src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import App from './App'
 
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+
+import catsReducer from './reducers/catsReducer'
+
+const store = createStore(catsReducer);
+
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>
+    ,
     document.getElementById('root')
 );
